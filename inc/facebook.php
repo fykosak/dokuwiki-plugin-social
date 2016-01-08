335d51d568e2b08480a3c9ef6e5f309362607692
Mišo <miso@fykos.cz>
2016-01-08 02:30:03 +0100

    conf & FB share and send

--- inc/facebook.php
+++ inc/facebook.php
@@ -1,39 +1,60 @@
 <?php
+
 /**
  * Description of sction
  *
  * @author Miso <miso@fykos.cz>
  */
-class helper_plugin_social_facebook extends DokuWiki_Plugin{
+class helper_plugin_social_facebook extends DokuWiki_Plugin {
+
     //put your code here
-    
-    
+
+
     public function __construct() {
         ;
     }
-    
-    public function ParseMatch($match){
+
+    public function ParseMatch($match) {
         preg_match('/{{FB-([a-z]*)>\s*(\S*)\s*(.*)}}/',$match,$matches);
         list(,$class,$href,$width) = $matches;
         return array('class' => $class,'href' => $href,'width' => $width);
-          
-          
+    }
+
+    public function CreateSend($href,$colorscheme = "light",$kid_directed_site = false,$ref = "") {
+        $r = "";
+        $r.= '<div class="fb-send" ';
+        $r.= ' data-href="'.htmlspecialchars($href).'" ';
+        $r.= ' data-colorscheme="'.$colorscheme.'" ';
+        if($kid_directed_site){
+            $r.=' data-kid-directed-site="true" ';
+        }
+        if($ref!=""){
+            $r.=' data-ref="'.htmlspecialchars($ref).'" ';
+        }
+        $r.='></div>';
+        return $r;
+    }
+
+    public function CreatePost($data) {
         
     }
-    public function CreateSend($data){
+
+    public function CreateLike($data) {
         
     }
-    public function CreatePost($data){
+
+    public function CreateShare($href,$layout="button") {
+        $r="";
+        $r.='<div class="fb-share-button" ';
+        $r.=' data-href="'.htmlspecialchars($href).'"  ';
+	$r.=' data-layout="'.$layout.'" ';	
+	$r.='></div>';
+        return $r;
         
     }
-    public function CreateLike($data){
+
+    public function CreatePage($data) {
         
     }
-    public function CreateShare($data){
-        
-    }
-    public function CreatePage($data){
-        
-    }
-    
+
 }
